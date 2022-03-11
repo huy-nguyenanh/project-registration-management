a8ddab4b46a24db1ddeca314be1d46bdaf75dbb8
Le Thanh Phuoc <thanhphuoc.se@gmail.com>
2022-03-11 20:00:02 +0700

    add template admin dashboard

---
+++ web/assets/js/main.js
@@ -0,0 +1,27 @@
+// $(document).ready(function () {
+//   $('a[data-toggle="tab"]').on("show.bs.tab", function (e) {
+//     localStorage.setItem("activeTab", $(e.target).attr("href"));
+//   });
+//   var activeTab = localStorage.getItem("activeTab");
+//   if (activeTab) {
+//     $('#myTab a[href="' + activeTab + '"]').tab("show");
+//   }
+// });
+
+// profile dropdown
+const profile = document.querySelector(".profile");
+const imgProfile = profile.querySelector("img");
+const dropdown = profile.querySelector(".profile-link");
+
+imgProfile.addEventListener("click", () => {
+  dropdown.classList.toggle("show");
+});
+
+window.addEventListener("click", function (e) {
+  if (e.target !== imgProfile) {
+    if (dropdown.classList.contains("show")) {
+      dropdown.classList.remove("show");
+    }
+  }
+});
+
