f63b2f0580316e234e59da85a6153b201d707db6
Le Thanh Phuoc <thanhphuoc.se@gmail.com>
2022-03-13 23:21:41 +0700

    add profile.html and update main.js

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,91 +1,107 @@
-// $(document).ready(function () {
-//   $('a[data-toggle="tab"]').on("show.bs.tab", function (e) {
-//     localStorage.setItem("activeTab", $(e.target).attr("href"));
-//   });
-//   var activeTab = localStorage.getItem("activeTab");
-//   if (activeTab) {
-//     $('#myTab a[href="' + activeTab + '"]').tab("show");
-//   }
-// });
+function dropdownProfile() {
+  // profile dropdown
+  const profile = document.querySelector(".profile");
+  const imgProfile = profile.querySelector("img");
+  const dropdown = profile.querySelector(".profile-link");
 
-// profile dropdown
-const profile = document.querySelector(".profile");
-const imgProfile = profile.querySelector("img");
-const dropdown = profile.querySelector(".profile-link");
+  imgProfile.addEventListener("click", () => {
+    dropdown.classList.toggle("show");
+  });
 
-imgProfile.addEventListener("click", () => {
-  dropdown.classList.toggle("show");
-});
-
-window.addEventListener("click", function (e) {
-  if (e.target !== imgProfile) {
-    if (dropdown.classList.contains("show")) {
-      dropdown.classList.remove("show");
-    }
-  }
-});
-
-// submit file on change
-const formImportFile = document.getElementById("import-form");
-const importFile = document.getElementById("import-file");
-importFile.addEventListener("change", () => {
-  formImportFile.submit();
-});
-
-//////
-const modals = document.querySelectorAll(".myModal");
-const btns = document.querySelectorAll(".myBtn");
-
-for (const btn of btns) {
-  for (const modal of modals) {
-    // btn.onclick = function (e) {
-    //   e.preventDefault();
-    //   modal.style.display = "block";
-    // };
-    btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      modal.style.display = "block";
-    });
-
-    const span = modal.querySelector(".close");
-    span.onclick = function () {
-      modal.style.display = "none";
-    };
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
+  window.addEventListener("click", function (e) {
+    if (e.target !== imgProfile) {
+      if (dropdown.classList.contains("show")) {
+        dropdown.classList.remove("show");
       }
-    };
-  }
+    }
+  });
 }
-
-// show error
-(() => {
-  const a0 = document.getElementById("error-modal");
-  const a1 = document.getElementById("myBtn");
-  const a2 = document.getElementsByClassName("close")[0];
-  const a3 = document.querySelector(".error");
-  if (a3.textContent !== "") {
-    a0.style.display = "block";
-    a3.textContent = a0.textContent;
-    a2.onclick = function () {
-      a0.style.display = "none";
-      a3.textContent = "";
-    };
-    window.onclick = function (event) {
-      if (event.target == modal) {
+function submitImportOnchange() {
+  // submit file on change
+  const formImportFile = document.getElementById("import-form");
+  const importFile = document.getElementById("import-file");
+  importFile.addEventListener("change", () => {
+    formImportFile.submit();
+  });
+}
+function editButton() {
+  const editBtn = document.querySelector(".edit-btn");
+  const editSpan = document.querySelector(".edit-span");
+  const editInput = document.getElementById("edit-input");
+  const formUpdateId = document.getElementById("updateId-form");
+  editBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (editSpan.style.display !== "none") {
+      editSpan.style.display = "none";
+      editInput.setAttribute("type", "text");
+      editBtn.textContent = "Update";
+    } else {
+      editSpan.style.display = "block";
+      editInput.setAttribute("type", "hidden");
+      editBtn.textContent = "Edit this post";
+      formUpdateId.submit();
+    }
+  });
+}
+function confirmSubmit() {
+  // confirm submit button
+  const submitBtn = document.querySelector(".confirm-submit");
+  submitBtn.addEventListener("click", () => {
+    if (!confirm("Are you sure ?")) {
+      return false;
+    }
+    this.form.submit();
+  });
+}
+function showErrorModal() {
+  // Error modal
+  (() => {
+    const a0 = document.getElementById("error-modal");
+    const a1 = document.getElementById("myBtn");
+    const a2 = document.getElementsByClassName("close")[0];
+    const a3 = document.querySelector(".error");
+    if (a3.textContent !== "") {
+      a0.style.display = "block";
+      a3.textContent = a0.textContent;
+      a2.onclick = function () {
         a0.style.display = "none";
         a3.textContent = "";
-      }
-    };
-  }
-})();
+      };
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          a0.style.display = "none";
+          a3.textContent = "";
+        }
+      };
+    }
+  })();
+}
+function showEditModal() {
+  //////
+  const modal = document.querySelector(".myModal");
+  const btn = document.querySelector(".myBtn");
+  // const span = modal.querySelector(".close");
 
-//
-const submitBtn = document.querySelector(".confirm-submit");
-submitBtn.addEventListener("click", () => {
-  if (!confirm("Are you sure ?")) {
-    return false;
-  }
-  this.form.submit();
-});
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(btn);
+    modal.style.display = "block";
+  });
+
+  // span.onclick = function () {
+  //   modal.style.display = "none";
+  // };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+}
+(() => {
+  dropdownProfile();
+  submitImportOnchange();
+  confirmSubmit();
+  showErrorModal();
+  showEditModal();
+  editButton();
+})();
